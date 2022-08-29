41b88ba80a66f27be18196f9c26df0091f42eb2e
afnfrhnaa <afnfrhnaa@gmail.com>
2022-08-29 15:40:12 +0800

    new

--- react-course-project/src/components/Layout/Header.js
+++ react-course-project/src/components/Layout/Header.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from "react";
 
-import mealImage from "../../assets/meal.jpeg";
+import mealsImage from "../../assets/meal.jpeg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>GoFood</h1>
         <button>Cart</button>
       </header>
-      <div>
-        <img />
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Delicious food!" />
       </div>
     </Fragment>
   );
