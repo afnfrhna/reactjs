66f7fd35655a4e1c105f6682e67112e652f06062
afnfrhnaa <afnfrhnaa@gmail.com>
2022-08-29 15:31:22 +0800

    new

---
+++ react-course-project/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+import mealImage from "../../assets/meal.jpeg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header>
+        <h1>GoFood</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
