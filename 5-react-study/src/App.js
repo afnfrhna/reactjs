7588847df3f16b05365abbd149f012d1a36e07ef
afnfrhnaa <afnfrhnaa@gmail.com>
2022-09-06 14:51:36 +0800

    new upd

--- 5-react-study/src/App.js
+++ 5-react-study/src/App.js
@@ -1,31 +1,45 @@
 import React, { useState, useCallback } from "react";
 
 import "./App.css";
+import DemoList from "./components/Demo/DemoList";
 import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/DemoOutput";
+// import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
 
-  console.log("APP RUNNING");
+  // console.log("APP RUNNING");
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  //   }
+  // }, [allowToggle]);
 
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  // const allowToggleHandler = () => {
+  //   setAllowToggle(true);
+  // };
+
+  // return (
+  //   <div className="app">
+  //     <h1>Hi there!</h1>
+  //     <DemoOutput show={false} />
+  //     <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+  //     <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+  //   </div>
+  // );
+
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={false} />
-      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
--- 5-react-study/src/components/UI/Button/Button.js
+++ 5-react-study/src/components/UI/Button/Button.js
@@ -3,6 +3,19 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
+  // return (
+  //   <button
+  //     type={props.type || "button"}
+  //     className={`${classes.button} ${props.className}`}
+  //     onClick={props.onClick}
+  //     disabled={props.disabled}
+  //   >
+  //     {props.children}
+  //   </button>
+  // );
+
+  console.log("BUTTON RUNNING");
+
   return (
     <button
       type={props.type || "button"}
