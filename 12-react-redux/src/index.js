e531c2c0dedabd9212648257fc987effd25a277c
afnfrhnaa <afnfrhnaa@gmail.com>
2022-09-14 14:32:52 +0800

    update

--- 12-react-redux/src/index.js
+++ 12-react-redux/src/index.js
@@ -4,5 +4,11 @@ import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
+import store from "./store/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
