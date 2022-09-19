28af43dc1e439a82fdd81e17df9b47ac61dd5d54
afnfrhnaa <afnfrhnaa@gmail.com>
2022-09-19 13:03:19 +0800

    update

---
+++ 15-react-router-project/src/pages/QuoteDetail.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
+      {/* <Route path={`quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route> */}
+      
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
